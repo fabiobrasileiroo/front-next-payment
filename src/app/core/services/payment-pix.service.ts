142be3b74695227006ba0c046b81d9655fa64902
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-09-16 13:42:35 -0400

    update services

--- src/app/core/services/payment-pix.service.ts
+++ src/app/core/services/payment-pix.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class PaymentService {
-  private apiUrl = 'http://localhost:3000/api/payments';
+  private apiUrl = 'https://back-next-payment.onrender.com/api/payments';
 
   constructor(private http: HttpClient) {}
 
